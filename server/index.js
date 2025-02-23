a99346a8257b0481c9ec14f7d69a9fe69f88ea8e
Asadullah Shah <ssyedasad079@gmail.com>
2025-02-24 00:11:06 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import cors from 'cors'
+import cors from 'cors';
 import { userRoute } from './routes/userRoute.js';
 import { ResidencyRoute } from './routes/residencyRoute.js';
 
@@ -9,15 +9,14 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 
-app.use('/api/user', userRoute)
-app.use('/api/residency', ResidencyRoute)
+app.use('/api/user', userRoute);
+app.use('/api/residency', ResidencyRoute);
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on Port ${PORT}`)
-})
+// This is the serverless function handler for Vercel
+export default function handler(req, res) {
+  app(req, res); // Pass the request and response objects to the Express app
+}
