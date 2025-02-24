748b1d9784f52503c7e287f893be6caaf5c9b657
Asadullah Shah <ssyedasad079@gmail.com>
2025-02-24 21:34:28 +0500

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from 'dayjs'
 import {toast} from 'react-toastify'
 
 export const api = axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: 'https://full-stack-website-f634.onrender.com'
 })
 
 export const getAllProperties = async()=>{
